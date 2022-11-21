9dfa420d1617e678510b3fc6163bca01a55d300f
Normand <normandlaredo@gmail.com>
2022-11-21 17:11:26 +0000

    Change key django

--- base/settings.py
+++ base/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-d4k#bt-^74^iqdv@_vbu630s)gc24r61sdb-@qgee1_9)7b(+-"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
