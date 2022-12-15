fff98a20e39ac75687655cbb63cacaf63f51f27e
Normand <normandlaredo@gmail.com>
2022-12-15 13:06:32 +0000

    Adding dotenv for deploying

--- base/settings.py
+++ base/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ""
+SECRET_KEY = str(os.getenv('SECRET_KEY '))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
