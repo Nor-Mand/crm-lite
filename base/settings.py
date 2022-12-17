ea0f3c70341f1121ec156d9c733ff33c48a0d29e
Normand <normandlaredo@gmail.com>
2022-12-17 13:55:57 +0000

    [FIX] Conection in production configuration

--- base/settings.py
+++ base/settings.py
@@ -108,6 +108,7 @@ DATABASES = {
 
 DATABASES['default'] = DATABASES['dev' if DEBUG else 'production']
 
+
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
