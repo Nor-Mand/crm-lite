88f3f2b2648194ace882c859c046f90d6fd6088f
Normand <normandlaredo@gmail.com>
2022-12-17 13:39:46 +0000

    [FIX] key: modified secret key for production

--- base/settings.py
+++ base/settings.py
@@ -23,7 +23,7 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY ')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
