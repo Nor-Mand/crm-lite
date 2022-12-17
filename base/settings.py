e6dc75749fa029ea1c1a8bf0edca3b7f294d3e7c
Normand <normandlaredo@gmail.com>
2022-12-17 21:15:13 +0000

    [ADD] collect static files to deploy in railway

--- base/settings.py
+++ base/settings.py
@@ -26,7 +26,7 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 # RAILWAY CONFIGURATION DEPLOY
@@ -142,7 +142,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "theme/static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
