48e4360be97bfc586ec89294700652323e933546
Normand <normandlaredo@gmail.com>
2022-12-18 01:06:53 +0000

    [ADD] setting static files in production

--- base/settings.py
+++ base/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "staticfiles/"
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
